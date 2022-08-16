7db6c3e6835891b3422d36e38a9d6a714e51c218
AngelERodriguezF <110403345+AngelERodriguezF@users.noreply.github.com>
2022-08-16 13:19:26 -0500

    Update exercise.py

--- assignments/01Peso/src/exercise.py
+++ assignments/01Peso/src/exercise.py
@@ -1,8 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    print('Hola')
-
-
+    peso_inicial = float(input('Dame el peso incial: '))
+    peso_final = float(input('Dame el peso final: '))
+    meses = int(input('Dame la cantidad de meses: '))
+    peso_mes = (peso_inicial-peso_final)/meses
+    print('Lo que debes bajar por mes es:',peso_mes)
 
 if __name__ == '__main__':
     main()
